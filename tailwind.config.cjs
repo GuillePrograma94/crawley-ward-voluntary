389d5a9ea3d2768c238ad8441b7a221afdcda663
GuillePrograma94 <guilleprograma94@gmail.com>
2024-10-03 14:44:47 +0100

    feat: Updated global.css for toggle button

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: 'class', // Add this line
     theme: {
         extend: {
             backgroundImage: {
@@ -21,24 +22,6 @@ export default {
     daisyui: {
         themes: [
             {
-                dark: {
-                    ...require('daisyui/src/theming/themes')['dark'],
-                    primary: '#ffae9c',
-                    'primary-content': '#171227',
-                    secondary: '#fff2d7',
-                    'secondary-content': '#171227',
-                    accent: '#e3f1ff',
-                    'accent-content': '#171227',
-                    neutral: '#2e293c',
-                    'neutral-content': '#ffffff',
-                    'base-100': '#171227',
-                    'base-200': '#2e293c',
-                    'base-content': '#ffffff',
-                    '--rounded-btn': '.375rem',
-                    '--rounded-badge': '9999px'
-                }
-            },
-            {
                 light: {
                     ...require('daisyui/src/theming/themes')['light'],
                     primary: '#ffae9c',
@@ -52,6 +35,24 @@ export default {
                     'base-100': '#ffffff',
                     'base-200': '#d7d8e4',
                     'base-content': '#171227',
+                    '--rounded-btn': '.375rem',
+                    '--rounded-badge': '9999px'
+                }
+            },
+            {
+                dark: {
+                    ...require('daisyui/src/theming/themes')['dark'],
+                    primary: '#ffae9c',
+                    'primary-content': '#171227',
+                    secondary: '#fff2d7',
+                    'secondary-content': '#171227',
+                    accent: '#e3f1ff',
+                    'accent-content': '#171227',
+                    neutral: '#2e293c',
+                    'neutral-content': '#ffffff',
+                    'base-100': '#171227',
+                    'base-200': '#2e293c',
+                    'base-content': '#ffffff',
                     '--rounded-btn': '.375rem',
                     '--rounded-badge': '9999px'
                 }
